608ea8cf532e599843fe1638f28d3a471e2dc3fe
VJ <vishnuvijay@factor5.com.au>
2019-03-13 20:55:52 +1100

    final node module tweaks

---
+++ src/Page/Calculator/Calculator.spec.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Calculator from './Calculator';
+import { shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+import * as E from './CalculatorStyle';
+
+configure({ adapter: new Adapter() });
+
+it('renders without crashing', () => {
+  shallow(<Calculator />);
+});
+
+it('renders without crashing', () => {
+  shallow(<E.CalculatorOperator />);
+});
+
+it('renders without crashing', () => {
+  shallow(<E.CalculatorNumberField />);
+});
+
+it('renders without crashing', () => {
+  shallow(<E.CalculatorResult />);
+});
+
+it('renders without crashing', () => {
+  shallow(<E.CalculatorContainer />);
+});
+
+test('Adding 1 + 1 equals 2', () => {
+  expect(1 + 1).toBe(2);
+});
+test('Multiplying 1 * 1 equals 1', () => {
+  expect(1 * 1).toBe(1);
+});
+test('Subtracting 1 - 1 equals 0', () => {
+  expect(1 - 1).toBe(0);
+});
+test('Dividing 1 / 1 equals 1', () => {
+  expect(1 / 1).toBe(1);
+});
