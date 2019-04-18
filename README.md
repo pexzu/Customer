4a7a9062aab01c68874232469a1791fd4b5e1255
VJ <vishnuvijay@factor5.com.au>
2019-04-19 03:40:38 +1000

    Updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-CalculatorApplication
+This is a Customer CRUD application developed to satisfy the demands for the coding test arranged by the recruitment team of Alinta energy.
+
+Date: 19th April 2019
+Author: Vishnu Vijay
+
+#Pre-requisites
+
+Need to yarn or npm all the packages used for this application.
+
+#Running the application
+
+yarn run start - will run all the tests and run the application in local server.
+
+#Built With
+
+emotion (for styled components) - CSS in JS library
+redux - for the global state managemet
+jest- for testing
+typescript- (workspace vertion 3.3.3333)
+
+#Limitations
+
+Due to time constraints, there are only minimal test cases and less validation.
+Additionally the code can be way optimised by breaking down the main page for the application 'Customer.tsx' into smaller functional components.
+
+Eventhough it is a small application, I have implemented redux store concept to exhibit my understanding about redux.
--- src/Main/Customer/Customer.spec.tsx
+++ src/Main/Customer/Customer.spec.tsx
@@ -12,31 +12,14 @@ it('renders without crashing', () => {
   shallow(<Customer />);
 });
 
-// it('renders without crashing', () => {
-//   shallow(<E.CalculatorOperator />);
-// });
-
-// it('renders without crashing', () => {
-//   shallow(<E.CalculatorNumberField />);
-// });
-
-// it('renders without crashing', () => {
-//   shallow(<E.CalculatorResult />);
-// });
-
-// it('renders without crashing', () => {
-//   shallow(<E.CalculatorContainer />);
-// });
-
-test('Adding 1 + 1 equals 2', () => {
-  expect(1 + 1).toBe(2);
+it('renders without crashing', () => {
+  shallow(<E.CustomerContainer />);
 });
-test('Multiplying 1 * 1 equals 1', () => {
-  expect(1 * 1).toBe(1);
+
+it('renders without crashing', () => {
+  shallow(<E.CustomerPrimaryButton />);
 });
-test('Subtracting 1 - 1 equals 0', () => {
-  expect(1 - 1).toBe(0);
-});
-test('Dividing 1 / 1 equals 1', () => {
-  expect(1 / 1).toBe(1);
+
+it('renders without crashing', () => {
+  shallow(<E.CustomerSecondaryButton />);
 });
